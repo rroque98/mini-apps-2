b344aa07a70eb0806d1ec8cf08f4876569665879
Ricardo <rroque98@gmail.com>
2018-09-10 16:41:06 -0700

    Implement basic keypad

--- challenge_3/client/src/components/App.jsx
+++ challenge_3/client/src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Pins from './Pins.jsx';
+import Scorecard from './Scorecard.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    console.log(e.target.value)
   }
 
   render() {
     return (
-      <div>HELLO WORLD</div>
+      <div>
+        <Pins handleClick={this.handleClick} />
+        <Scorecard />
+      </div>
     );
   }
 }
---
+++ challenge_3/client/src/components/Pins.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Pins = ({ handleClick, }) => (
+  <div>
+    <div>
+      <button type="button" value={1} onClick={handleClick}>1</button>
+      <button type="button" value={2} onClick={handleClick}>2</button>
+      <button type="button" value={3} onClick={handleClick}>3</button>
+      <button type="button" value={4} onClick={handleClick}>4</button>
+      <button type="button" value={5} onClick={handleClick}>5</button>
+    </div>
+    <div>
+      <button type="button" value={6} onClick={handleClick}>6</button>
+      <button type="button" value={7} onClick={handleClick}>7</button>
+      <button type="button" value={8} onClick={handleClick}>8</button>
+      <button type="button" value={9} onClick={handleClick}>9</button>
+      <button type="button" value={10} onClick={handleClick}>10</button>
+    </div>
+  </div>
+)
+
+export default Pins;
